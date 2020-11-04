9fc4aa7a3d9a6e15fa936caec1600ad06939e424
Chum <ravichum@gmail.com>
2020-11-04 01:56:48 -0800

    Problem 3 finished

--- src/main/java/Problem1/Permutation.java
+++ src/main/java/Problem1/Permutation.java
@@ -6,6 +6,29 @@ import java.util.List;
 public class Permutation {
     public static List<List<Integer>> permute(int[] nums) {
         // homework
-        return new ArrayList<>(); // place holder
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        boolean[] used = new boolean[nums.length];
+        List<Integer> permutation = new ArrayList<>();
+        helper(nums, permutation, used, result);
+        return result;
+    }
+
+    private static void helper(int[] nums, List<Integer> permutation, boolean[] used, List<List<Integer>> result) {
+        if (permutation.size() == nums.length) {
+            result.add(new ArrayList<>(permutation));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i])
+                continue;
+            used[i] = true;
+            permutation.add(nums[i]);
+            helper(nums, permutation, used, result);
+            permutation.remove(permutation.size() - 1);
+            used[i] = false;
+        }
     }
 }
--- src/main/java/Problem1/ReverseStack.java
+++ src/main/java/Problem1/ReverseStack.java
@@ -2,7 +2,7 @@ package Problem1;
 
 import java.util.Stack;
 /*
-
+How I understood to do this problem
 https://algorithms.tutorialhorizon.com/reverse-a-stack-using-recursion-in-place-without-using-extra-memory/
 http://liesbeek.com/2016/06/22/reverse-stack-using-recursion/
 https://discuss.codechef.com/t/reverse-a-stack-using/13596/3
